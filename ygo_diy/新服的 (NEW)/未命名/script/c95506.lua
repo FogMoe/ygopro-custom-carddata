f9bcb383dca917c71701a5c8030dca736b705e3b
Kc <kc@fog.moe>
2022-06-09 07:47:18 +0800

    1

--- ygo_diy/新服的 (NEW)/未命名/script/c95506.lua
+++ ygo_diy/新服的 (NEW)/未命名/script/c95506.lua
@@ -2,5 +2,75 @@ local m=95506
 local cm=_G["c"..m]
 cm.name="机械化星球"
 function cm.initial_effect(c)
-	
+		--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+		--
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(m,0))
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetHintTiming(TIMING_DAMAGE_STEP)
+	e3:SetCountLimit(1,m)
+	e3:SetCondition(cm.thcon)
+	e3:SetTarget(cm.thtg)
+	e3:SetOperation(cm.thop)
+	c:RegisterEffect(e3)
+	--Activate
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetHintTiming(TIMING_END_PHASE)
+	e2:SetCondition(cm.condition)
+	e2:SetOperation(cm.activate)
+	c:RegisterEffect(e2)
+end
+function cm.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()==PHASE_END
+end
+function cm.activate(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(0,1)
+	e1:SetValue(cm.aclimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function cm.aclimit(e,re,tp)
+	return re:IsActiveType(TYPE_SPELL+TYPE_TRAP+TYPE_MONSTER)
+end
+
+
+
+function cm.cfilter(c,tp)
+	return c:IsType(TYPE_MONSTER) and c:IsReason(REASON_EFFECT) and c:IsSetCard(0x9901)
+end
+function cm.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(cm.cfilter,1,nil,tp)
+end
+function cm.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0x9901)
+end
+function cm.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(cm.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+end
+function cm.thop(e,tp,eg,ep,ev,re,r,rp)
+	local sg=Duel.GetMatchingGroup(cm.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,e)
+	local c=e:GetHandler()
+	local tc=sg:GetFirst()
+	while tc do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(400)
+		tc:RegisterEffect(e1)
+		tc=sg:GetNext()
+	end
 end
