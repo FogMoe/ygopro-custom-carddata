d412035fb155cfda0ef4964db7545b48708d9d49
Kc <kc@fog.moe>
2022-06-09 09:42:27 +0800

    1

--- ygo_diy/新服的 (NEW)/机械化卡包/script/c95508.lua
+++ ygo_diy/新服的 (NEW)/机械化卡包/script/c95508.lua
@@ -2,6 +2,11 @@ local m=95508
 local cm=_G["c"..m]
 cm.name="机械化遗迹"
 function cm.initial_effect(c)
+		--Activate
+	local e9=Effect.CreateEffect(c)
+	e9:SetType(EFFECT_TYPE_ACTIVATE)
+	e9:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e9)
 		--Normal monster
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -10,4 +15,11 @@ function cm.initial_effect(c)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetValue(TYPE_MONSTER)
 	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_ADD_TYPE)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetValue(TYPE_NORMAL)
+	c:RegisterEffect(e2)
 end
