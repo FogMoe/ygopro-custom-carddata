9d1915176c94bc82a7cc17554192b52d3c6c39ce
Kc <kc@fog.moe>
2022-06-09 10:20:18 +0800

    1

--- ygo_diy/新服的 (NEW)/机械化卡包/script/c95508.lua
+++ ygo_diy/新服的 (NEW)/机械化卡包/script/c95508.lua
@@ -12,14 +12,64 @@ function cm.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetCode(EFFECT_ADD_TYPE)
-	e1:SetRange(LOCATION_HAND)
+	e1:SetRange(LOCATION_HAND+LOCATION_MZONE)
 	e1:SetValue(TYPE_MONSTER)
 	c:RegisterEffect(e1)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e2:SetCode(EFFECT_ADD_TYPE)
-	e2:SetRange(LOCATION_HAND)
+	e2:SetRange(LOCATION_HAND+LOCATION_MZONE)
 	e2:SetValue(TYPE_NORMAL)
 	c:RegisterEffect(e2)
+	--act limit
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(0,1)
+	e3:SetCondition(cm.actcon)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+	--discard
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(m,0))
+	e4:SetCategory(CATEGORY_TOGRAVE)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetRange(LOCATION_GRAVE)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetCost(aux.bfgcost)
+	e4:SetTarget(cm.distg)
+	e4:SetCondition(cm.setcon)
+	e4:SetHintTiming(TIMING_END_PHASE)
+	e4:SetOperation(cm.disop)
+	c:RegisterEffect(e4)
+end
+function cm.setcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()==PHASE_END
+end
+function cm.actfilter(c,tp)
+	return c:IsFaceup() and c:IsSetCard(0x9901) and c:IsControler(tp)
+end
+function cm.actcon(e)
+	local tp=e:GetHandlerPlayer()
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	return (a and cm.actfilter(a,tp)) or (d and cm.actfilter(d,tp))
+end
+
+
+function cm.disfilter(c)
+	return c:IsSetCard(0x9901) and c:IsType(TYPE_MONSTER) and c:IsAbleToGrave()
+end
+function cm.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(cm.disfilter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,0,0)
+end
+function cm.disop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,cm.disfilter,tp,LOCATION_HAND,0,1,1,nil)
+	if g:GetCount()>0 then Duel.SendtoGrave(g,REASON_EFFECT) end
 end
