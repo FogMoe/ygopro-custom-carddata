6e79c766b71a7f2a33dda071a776195bc4157387
Kc <kc@fog.moe>
2022-06-09 13:04:37 +0800

    2

---
+++ ygo_diy/新服的 (NEW)/机械化卡包/script/c95509.lua
@@ -0,0 +1,46 @@
+local m=95509
+local cm=_G["c"..m]
+cm.name="机械化"
+function cm.initial_effect(c)
+		--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(cm.target)
+	e1:SetCondition(cm.condition)
+	e1:SetOperation(cm.activate)
+	c:RegisterEffect(e1)
+end
+function cm.filter1(c)
+	return c:IsFaceup() and c:IsSetCard(0x9901) and c:IsType(TYPE_MONSTER)
+end
+function cm.condition(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.IsExistingMatchingCard(cm.filter1,tp,LOCATION_MZONE,0,1,nil)
+end
+function cm.cfilter(c)
+	return c:IsSetCard(0x9901) and c:IsAbleToHand()
+end
+function cm.cfilter2(c)
+	return c:IsSetCard(0x9901) and c:IsAbleToGrave()
+end
+function cm.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and chkc:IsAbleToHand() end
+	if chk==0 then return Duel.IsExistingTarget(cm.cfilter,tp,LOCATION_GRAVE,0,1,nil) and Duel.IsExistingMatchingCard(cm.cfilter2,tp,LOCATION_DECK,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,cm.cfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function cm.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+		local g=Duel.SelectMatchingCard(tp,cm.cfilter2,tp,LOCATION_DECK,0,1,1,nil)
+		if g:GetCount()>0 then
+			Duel.SendtoGrave(g,REASON_EFFECT)
+		end
+	end
+end
